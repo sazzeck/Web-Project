397a8b087585e904d06b16a7f134d3f03fcb562f
sazzeck <sazzeckprog@gmail.com>
2022-07-04 23:21:26 +0300

    add pages auth, add urls, add views

--- app/main/urls.py
+++ app/main/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.main, name="main"),
     path('test', views.test_page, name="test"),
+    path('login', views.login_page, name="sing_in"),
+    path('registration', views.registration_page, name="sing_up"),
 ]
--- app/main/views.py
+++ app/main/views.py
@@ -8,3 +8,15 @@ def main(request):
 
 def test_page(request):
     return render(request, "main/test.html", {"title": "Test"})
+
+
+def authentication(request):
+    return render(request, "main/auth.html", {"title": "Authentication"})
+
+
+def login_page(request):
+    return render(request, "main/login.html", {"title": "Login"})
+
+
+def registration_page(request):
+    return render(request, "main/registration.html", {"title": "Registration"})
