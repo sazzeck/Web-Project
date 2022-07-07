b22e3105af3e1c6746c78c8cc7296cb92fcf5992
sazzeck <sazzeckprog@gmail.com>
2022-07-07 23:10:41 +0300

    update models

--- app/main/admin.py
+++ app/main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Services, Worker, Location
 
 # Register your models here.
+admin.site.register(Services)
+admin.site.register(Worker)
+admin.site.register(Location)
--- app/main/models.py
+++ app/main/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 
 
 class Services(models.Model):
     name = models.CharField(max_length=100, db_index=True, verbose_name="Название")
+    parent = models.ForeignKey("self", null=True, blank=True, on_delete=models.PROTECT)
 
     class Meta:
         verbose_name = "Услуга"
@@ -12,32 +15,15 @@ class Services(models.Model):
         ordering = ["name", "id"]
 
     def __str__(self):
-        return f"{self.name}"
-
-
-class Schedule(models.Model):
-    worker = None
-    time = None
-
-
-class Location(models.Model):
-    name = models.CharField(max_length=100, db_index=True, verbose_name="Название")
-
-    class Meta:
-        verbose_name = "Локация"
-        verbose_name = "Локации"
-        ordering = ["name", "id"]
+        return f"{self.name} ({self.parent})" if self.parent else self.name
 
 
 class Worker(models.Model):
     service = models.ForeignKey(
-        Services, on_delete=models.PROTECT, null=True, verbose_name="Услуга"
+        Services, on_delete=models.CASCADE, null=True, verbose_name="Услуга"
     )
-    first_name = models.CharField(max_length=30, verbose_name="Имя")
-    last_name = models.CharField(max_length=30, verbose_name="Фамилия")
-    location = models.ForeignKey(
-        Location, on_delete=models.PROTECT, null=True, verbose_name="Локация"
-    )
+    location = models.ForeignKey("Location", on_delete=models.PROTECT, null=True, verbose_name="Локация")
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     is_active = models.BooleanField(default=True, verbose_name="Активен")
 
     class Meta:
@@ -46,4 +32,21 @@ class Worker(models.Model):
         ordering = ["service", "location", "id", "is_active"]
 
     def __str__(self):
-        return f"Имя: {self.first_name}; Фамилия: {self.last_name}; Услуга: {self.service}"
+        return f"{self.user} ({self.location}) - {self.service}"
+
+
+class Location(models.Model):
+    name = models.CharField(max_length=100, db_index=True, verbose_name="Название")
+
+    class Meta:
+        verbose_name = "Локация"
+        verbose_name_plural = "Локации"
+        ordering = ["name", "id"]
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+# class Schedule(models.Model):
+#     worker = models.ForeignKey(Worker, on_delete=models.CASCADE)
+#     time = None
