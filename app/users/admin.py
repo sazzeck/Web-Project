84955864abc9491f7aa3bd3f0b0d41a21fad54b8
sazzeck <sazzeckprog@gmail.com>
2022-07-26 21:24:00 +0300

    admin panel customization

--- app/users/admin.py
+++ app/users/admin.py
@@ -1,7 +1,72 @@
-from django.contrib import admin
+from django.contrib.admin import register, ModelAdmin
 from .models import Users, Worker, Customer
 
 # Register your models here.
-admin.site.register(Users)
-admin.site.register(Worker)
-admin.site.register(Customer)
+
+
+@register(Users)
+class UsersAdmin(ModelAdmin):
+    list_display = (
+        "username",
+        "user_type",
+        "is_online",
+        "is_active",
+        "is_staff",
+        "date_joined",
+    )
+
+    list_display_links = ("username",)
+
+    search_fields = ("username",)
+
+    list_filter = (
+        "user_type",
+        "is_online",
+        "is_active",
+        "is_staff",
+    )
+
+    list_editable = (
+        "is_active",
+        "is_staff",
+    )
+
+    show_full_result_count = True
+
+
+@register(Worker)
+class WorkerAdmin(ModelAdmin):
+    list_display = (
+        "username",
+        "is_online",
+        "date_joined",
+    )
+
+    list_display_links = ("username",)
+
+    search_fields = ("username",)
+
+    list_filter = (
+        "is_online",
+    )
+
+    show_full_result_count = True
+
+
+@register(Customer)
+class CustomerAdmin(ModelAdmin):
+    list_display = (
+        "username",
+        "is_online",
+        "date_joined",
+    )
+
+    list_display_links = ("username",)
+
+    search_fields = ("username",)
+
+    list_filter = (
+        "is_online",
+    )
+
+    show_full_result_count = True
